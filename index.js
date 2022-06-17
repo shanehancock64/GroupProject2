c2d8d05759501b8c7469f476252177fc0a47abfa
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-06-16 23:15:57 -0500

    index.js updates

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
-var session = require('express-session');
-var SQLiteStore = require('connect-sqlite3')(session);
-var passport = require('passport');
-var indexer = require('./routes/index');
-var auths = require('./routes/passport');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+// var SQLiteStore = require('connect-sqlite3')(session);
+// var passport = require('passport');
+// var indexer = require('./routes/index');
+// var auths = require('./routes/passport');
 
-const express = require("express");
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
-const port = 3000;
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(session({
-  secret: 'chucky cheese',
-  resave: false,
-  saveUninitialized: false,
-  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
-}));
-app.use(passport.authenticate('session'));
-app.use('/', indexRouter);
-app.use('/', authRouter);
+const PORT = process.env.PORT || 3001;
+// app.use(express.json());
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   })
+// );
 
-app.get("/", (req, res) => {
-  res.json({ message: "ok" });
+const sess = {
+  secret: 'chucky cheese',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({ 
+    db: sequelize})
+};
+
+app.use(session(sess));
+// app.use(passport.authenticate('session'));
+// app.use('/', indexer);
+// app.use('/', auths);
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "ok" });
+// });
+
+// app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
-app.listen(port, () => {
-  console.log(`Group Project listening at http://localhost:${port}`);
-}); 
