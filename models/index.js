06239234a85fc30019c7e6348b4aa6ca6f63cc60
Matt Estes <me6384@gmail.com>
2022-06-20 10:46:18 -0700

    cart models

--- models/index.js
+++ models/index.js
@@ -2,6 +2,9 @@ const User = require('./User');
 const Product = require('./Product');
 const Cart = require('./cart')
 
+//User can have many products and carts
+//Product can have many carts
+
 User.hasMany(Product, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
@@ -11,14 +14,22 @@ Product.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Cart.belongsTo(
-  models.Product
-);
+Product.hasMany(Cart, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+});
+
+Cart.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+
+User.hasMany(Cart, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
 Cart.belongsTo(User, {
-  foreignKey: {
-    allowNull: false
-  }
+  foreignKey: 'user_id'
 });
 
 module.exports = { User, Product, Cart };
