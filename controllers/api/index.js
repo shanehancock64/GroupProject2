fc35a6b6d58e6a4f2040abf64469bc3326843fd3
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-06-21 10:46:01 -0500

    routes updates 1000

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const productRoutes = require('./productRoutes');
+const orderRoutes = require('./orderRoutes');
+const cartRoutes = require('./cartRoutes');
 
 router.use('/users', userRoutes);
 router.use('/products', productRoutes);
+router.use('/order', orderRoutes);
+router.use('/cart', cartRoutes);
 
 module.exports = router;
